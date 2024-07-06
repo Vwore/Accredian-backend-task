4d00eb89a800bc87a0bbd66de8b140be751ac768
Vcogo <vanshaj.wore@cogoport.com>
2024-07-06 23:33:42 +0530

    database changed

--- prisma/migrations/20240706180219_init/migration.sql
+++ prisma/migrations/20240706180219_init/migration.sql
@@ -1,9 +1,10 @@
 -- CreateTable
 CREATE TABLE `Referral` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `name` VARCHAR(191) NOT NULL,
-    `email` VARCHAR(191) NOT NULL,
-    `message` VARCHAR(191) NULL,
+    `RefererName` VARCHAR(191) NOT NULL,
+    `RefererEmail` VARCHAR(191) NOT NULL,
+    `RefreeName` VARCHAR(191) NULL,
+    `RefreeEmail` VARCHAR(191) NULL,
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
 
     PRIMARY KEY (`id`)
